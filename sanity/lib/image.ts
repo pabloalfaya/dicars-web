77299f92606d9fd7acb7f016a63ddbd69749e026
pabloalfaya <pabloalfaya1@gmail.com>
2026-06-09 19:20:15 +0200

    Fase 2: integración de Sanity

--- sanity/lib/image.ts
+++ sanity/lib/image.ts
@@ -1,8 +1,8 @@
-import imageUrlBuilder from "@sanity/image-url";
-import type { SanityImageSource } from "@sanity/image-url/lib/types/types";
+import { createImageUrlBuilder } from "@sanity/image-url";
+import type { SanityImageSource } from "@sanity/image-url";
 import { dataset, projectId } from "@/sanity/env";
 
-const builder = imageUrlBuilder({ projectId, dataset });
+const builder = createImageUrlBuilder({ projectId, dataset });
 
 export function urlForImage(source: SanityImageSource) {
   return builder.image(source).auto("format").fit("max");
